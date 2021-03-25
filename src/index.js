684be3acc15d0480fe190afbdc1de43a89d48682
Tanya Kalkuta <Tanyakalkuta@gmail.com>
2021-03-25 20:15:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './styles.css';
+// import './styles.css';
+//import './template/template'
+// import menuList from './menu.json'
+
+// const menuItem = menuList[0];
+// const menuRef = document.querySelector('.js-menu');
+
+// console.log(menuItem);
+// console.log(menuRef);
+// menuRef.insertAdjacentHTML('beforeend', menuItem);
+
+console.log(qweqwe);
